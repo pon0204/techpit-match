3bd456202cfb9bc4d467a3a7a51a4557372af2cf
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2021-05-23 11:38:29 +0900

    Font Awesome導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 import 'bootstrap';
 import '../stylesheets/application';
+import '@fortawesome/fontawesome-free/js/all';
 
 require("@rails/ujs").start()
 require("turbolinks").start()
